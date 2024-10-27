7418e4dd3f47709b9a60c2528994e44d98b63545
oliver <oliverbcontact@gmail.com>
2024-10-27 14:23:04 -0400

    minor tweaks
    added inter font, setup basic js, and other tiny tweaks

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from app import create_app
 
 app = create_app()
+PORT = 5500
+HOST = '0.0.0.0'
 
 if __name__ == '__main__':
     app.run(
         debug=True,
-        port=5500,
-        host='0.0.0.0'
+        port=PORT,
+        host=HOST
     )
